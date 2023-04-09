84b83d534ec6d370d16fdf9100684db5f7f3ee8e
Badea Dragos <dragos_stefan_13@yahoo.com>
2023-04-09 05:01:26 +0300

    import

--- app/src/main/java/com/example/sendvis2/HomeFragment.java
+++ app/src/main/java/com/example/sendvis2/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.sendvis2;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,6 +12,12 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.time.LocalTime;
 
